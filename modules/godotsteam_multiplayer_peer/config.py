a7e8be0cd738f3553af7e917df6a891584f2a55d
SchnozzleCat <linus@schnozzlecat.xyz>
2026-01-28 17:44:58 +0100

    feat: add steam integration

---
+++ modules/godotsteam_multiplayer_peer/config.py
@@ -0,0 +1,16 @@
+# config.py
+
+def can_build(env, platform):
+	return platform=="linuxbsd" or platform=="windows" or platform=="macos" or platform=="server"
+
+def configure(env):
+	pass
+
+def get_doc_classes():
+	return [
+		"SteamMultiplayerPeer",
+		"SteamPacketPeer"
+	]
+
+def get_doc_path():
+	return "doc_classes"
